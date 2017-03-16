050937961afbb5a53708e198e4d934669721a176
BERGOT Gaëtan <bergot.gaetan01@gmail.com>
2017-03-16 18:24:23 +0100

    Add files via upload

---
+++ aide texte html semantic ui.php
@@ -0,0 +1,20 @@
+<?php
+function uiHeader($title){
+?>
+<!doctype html>
+<html>
+<head>
+	<title><?php echo $title;?></title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+</head>
+<body>
+	<div class="ui countainer"><?php
+}
+function uiFooter{
+	?>
+	</div>
+	<script src="js/semantic.min.js">
+</body>
+</html><?php
+}
